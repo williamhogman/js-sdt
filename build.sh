7c35818abc9a066f0379e817cb2297fa2a9b75f7
William Hogman <me@whn.se>
2012-04-09 17:42:42 +0200

    added build-script

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# Build script for js-sdt
+# 
+
+echo "Building js-sdt"
+
+type coffee >/dev/null 2>&1 || {
+    echo "Missing CoffeeScript, halting"
+    exit 1
+}
+
+echo "Compiling"
+coffee -o dist -c sdt.coffee 
+
+coffee_exit=$?
+
+if [[ $coffee_exit != 0 ]] ; then
+    echo "Compilation failed"
+    exit 1
+else
+    echo "Compilation successful"
+fi
+
+type yuicompressor >/dev/null 2>&1 && {
+    echo "Compressing using yuicompressor"
+    yuicompressor -o dist/sdt.min.js dist/sdt.js
+}
+
+cp vendor/jstat* dist/
+cp vendor/jquery.flot* dist/
+
+echo "Done!"
